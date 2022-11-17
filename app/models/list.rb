ecbe63483e005a9f9c54ae81f35e968a500618ba
marine irigoin <marine.irigoin@gmail.com>
2022-11-17 11:26:14 +0100

    create model movie and list

---
+++ app/models/list.rb
@@ -0,0 +1,4 @@
+class List < ApplicationRecord
+  has_many :bookmarks
+  validates :name, presence: true, uniqueness: true
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+"Creating 10 fakes movies with random quotes ..."
+10.times do
+  Movie.create(title: Faker::Movie.title,
+    overview: Faker::Movie.quote,
+    poster_url: Faker::LoremFlickr.image,
+    rating: rand(0..10))
+end
+"Done !"
